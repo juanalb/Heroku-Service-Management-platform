8b7986cf7f1508c363f04225a65d5c896c180ec9
juanalb <juan@zimmermanzimmerman.com>
2020-10-21 23:37:34 +0200

    initial commit

---
+++ build/precache-manifest.0ba11c8f53bae2ec883bffe72a92914a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dc82bd27d2aae42906f5fb4eedd86a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af9683c4dd331c5c5c4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4754e27789876198beb7",
+    "url": "/static/js/2.a6a49937.chunk.js"
+  },
+  {
+    "revision": "5cf0b55f711b4765b7e42faa5df6636d",
+    "url": "/static/js/2.a6a49937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af9683c4dd331c5c5c4",
+    "url": "/static/js/main.ad278d7a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
